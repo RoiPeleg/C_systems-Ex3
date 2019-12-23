c5b642a592377800e9b013dc4ae71acf9cde6b5d
Live session user <roi2307@gmail.com>
2019-12-23 15:40:23 +0000

    fixed override

--- txtfind.c
+++ txtfind.c
@@ -66,10 +66,51 @@ int similar(char *s, char *t, int n)
 }
 void print_lines(char *str)
 {
+    char s[LINE] = {'0'};
+    while (s != 'EOF')
+    {
+        if (substring(*s, *str))
+        {
+            printf("%s", s);
+        }
+
+        getLine(*s);
+    }
 }
 void print_similar_words(char *str)
 {
+    char s[WORD] = {'0'};
+    while (s != 'EOF')
+    {
+        if (similar(*s, *str, 1))
+        {
+            printf("%s", s);
+        }
+        getWord(*s);
+    }
 }
 int main()
 {
+    char s[WORD] = {};
+    char *ptr = s;
+    getWord(ptr);
+    char *wordtosearch = *s;
+    *ptr = s;
+    getWord(ptr);
+    char option = *s;
+    getLine(s);
+    if (option = 'a')
+    {
+        print_lines(wordtosearch);
+    }
+    else if (option = 'b')
+    {
+        print_similar_words(wordtosearch);
+    }
+    else
+    {
+        printf("%c is not valid", option);
+    }
+
+    return 0;
 }
